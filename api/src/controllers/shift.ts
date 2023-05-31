3a4d600ae5348cdf37eba63c262d397631319658
NasosBoras <borasnasos@gmail.com>
2023-05-31 18:04:31 +0300

    Added joins.

--- api/src/controllers/shift.ts
+++ api/src/controllers/shift.ts
@@ -5,10 +5,13 @@ import Shift from "../models/shift"
 const table = "Shifts"
 
 export async function selectShiftId(req: Request, res: Response) {
-    const { id } = req.params
+    const { id } = req.query
+    if (!id) return res.json({ error: "No id specified" })
 
     try {
-        const shift = await selectById(parseInt(id), table)
+        const shift = await selectById(parseInt(id as string), table, `id,day,duration,userA:Users!Shifts_personA_fkey(id,username),
+        userB:Users!Shifts_personB_fkey(id,username),
+        userC:Users!Shifts_personC_fkey(id,username)`)
         return res.json({ msg: shift });
     } catch (e) {
         console.log(e)
@@ -20,7 +23,16 @@ export async function selectShiftId(req: Request, res: Response) {
 }
 
 export async function selectShift(req: Request, res: Response) {
-    return res.json({ msg: await select_all(table) })
+    try {
+        return res.json({
+            msg: await select_all(table, `id,day,duration,userA:Users!Shifts_personA_fkey(id,username),
+        userB:Users!Shifts_personB_fkey(id,username),
+        userC:Users!Shifts_personC_fkey(id,username)`)
+        })
+    } catch (e) {
+        console.log(e)
+        return res.json({ error: "Error returning shifts" })
+    }
 }
 
 export async function insertShift(req: Request, res: Response) {
@@ -42,9 +54,9 @@ export async function insertShift(req: Request, res: Response) {
 }
 
 export async function removeShift(req: Request, res: Response) {
-    const { id } = req.params
+    const { id } = req.query
     try {
-        await remove(parseInt(id), table)
+        await remove(parseInt(id as string), table)
     } catch (e) {
         console.log(e)
         return res.json({
@@ -55,10 +67,10 @@ export async function removeShift(req: Request, res: Response) {
 }
 
 export async function updateShift(req: Request, res: Response) {
-    const { id } = req.params
+    const { id } = req.query
     const shift: Shift = req.body
     try {
-        const data = await update(parseInt(id), shift, table)
+        const data = await update(parseInt(id as string), shift, table)
         return res.json({ msg: data }).status(200)
     } catch (e) {
         console.log(e)
