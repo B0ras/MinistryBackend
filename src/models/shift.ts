3ec83847e066c4968d644eff903f78bbdac9e06c
NasosBoras <borasnasos@gmail.com>
2023-07-01 20:46:18 +0300

    Replaced all interfaces with types & changed place param.

--- src/models/shift.ts
+++ src/models/shift.ts
@@ -1,11 +1,18 @@
-interface Shift {
+
+// interface Place {
+//     place: string
+// }
+
+import Place from './place'
+
+type Shift = {
     id: number
     day: Date,
     duration: number,
     personA?: number,
     personB?: number,
     personC?: number,
-    place?: string
+    place?: Omit<Place, "id">
 }
 
 export default Shift
