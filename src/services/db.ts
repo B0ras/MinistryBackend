a1c0019d0c737226ccd455439525b1b3eee66e37
NasosBoras <borasnasos@gmail.com>
2023-07-27 22:05:48 +0300

    Improved functions

--- src/services/db.ts
+++ src/services/db.ts
@@ -23,17 +23,18 @@ export async function select_all(table: string, parameters?: string) {
     return data;
 }
 
-export async function selectById(id: number, table: string, parameters?: string) {
+export async function selectById<T>(id: number, table: string, parameters?: string) {
     const { data, error } = await supabase
         .from(table)
         .select(parameters)
         .eq("id", id)
+        .returns<T>()
 
     if (error) throw error
-    return data;
+    return data[0];
 }
 
-export async function selectByUsername(username: string, table: string) {
+export async function selectByUsername(username: string, table: string, parameters?: string) {
     const { data, error } = await supabase
         .from(table)
         .select()
@@ -44,18 +45,18 @@ export async function selectByUsername(username: string, table: string) {
     return data[0];
 }
 
-export async function update(id: number, obj, table: string) {
+export async function update(id: number, obj, table: string, parameters?: string) {
     const { data, error } = await supabase
         .from(table)
         .update(obj)
         .eq("id", id)
-        .select()
+        .select(parameters)
 
     if (error) throw error
     return data
 }
 
-export async function remove(id: number, table: string) {
+export async function remove(id: number, table: string, parameters?: string) {
     const { data } = await supabase
         .from(table)
         .delete()
