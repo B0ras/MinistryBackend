780dd32e51e8a7dc683367b006644dfa2639231a
NasosBoras <borasnasos@gmail.com>
2023-07-31 13:32:36 +0300

    Added Delete Controller

--- src/routes/sign.ts
+++ src/routes/sign.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { signController } from '../controllers/sign';
+import { signToShift, signOutOfShift } from '../controllers/sign';
 
 const signRouterId = Router()
 
-signRouterId.put("/", signController)
+signRouterId.put("/", signToShift)
+signRouterId.delete("/", signOutOfShift)
 
 export default signRouterId
