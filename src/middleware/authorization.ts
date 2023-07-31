30bd22d602571f7e0e34e7f8e931a8366ff8e196
NasosBoras <borasnasos@gmail.com>
2023-07-31 13:58:21 +0300

    Added HasRole middleware

---
+++ src/middleware/authorization.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express"
+import { verifyJWT } from '../utils/jwt'
+import { selectById } from '../services/db'
+import User from '../models/user'
+
+
+const table = "Users"
+
+export function hasRole(role: number[]) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies["SESSION"]
+
+        if (!token) return res.status(401).json({ error: "Unauthorized" })
+
+        const jwtPayload = verifyJWT(token).sub
+
+        const userId = parseInt(jwtPayload as string)
+
+        const user: User = await selectById<User>(userId, table)
+
+        if (!user.role) return res.status(401).json({ error: "Unauthorized" })
+
+        if (role.includes(user.role)) return next()
+
+        return res.status(401).json({ error: "Unauthorized" })
+
+    }
+}
