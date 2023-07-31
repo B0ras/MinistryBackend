f6601770c3147c41acd534f802d4a3cdb13a1127
NasosBoras <borasnasos@gmail.com>
2023-07-31 18:12:00 +0300

    Added no token found Error

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -10,7 +10,7 @@ export function hasRole(role: number[]) {
     return async (req: Request, res: Response, next: NextFunction) => {
         const token = req.cookies["SESSION"]
 
-        if (!token) return res.status(401).json({ error: "Unauthorized" })
+        if (!token) return res.status(401).json({ error: "No token found" })
 
         const jwtPayload = verifyJWT(token).sub
 
